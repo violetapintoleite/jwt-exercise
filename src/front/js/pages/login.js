4d8c26946c318d57c3a68cd9bafcf49ca8c4b98d
Violeta Pinto Leite <violetalopesalves@gmail.com>
2022-06-29 11:39:04 +0000

    Fixed cors and finished /token request

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -7,22 +7,23 @@ export const Login = () => {
   const { store, actions } = useContext(Context);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  console.log("This is your token" + token);
-  const token = sessionStorage.getItem("token");
+  console.log("This is your token" + store.token);
+
   const history = useHistory();
 
   const handleClick = () => {
-    actions.login(email, password).then(() => {
-    history.push("/");
-    });
+    actions.login(email, password);
   };
+
+  if (store.token && store.token != "" && store.token != undefined)
+    history.push("/");
 
   return (
     <div className="text-center mt-5">
       <h1>Login</h1>
 
-      {token && token != "" && token != undefined ? (
-        "You are logged in with this token" + token
+      {store.token && store.token != "" && store.token != undefined ? (
+        "You are logged in with this token" + store.token
       ) : (
         <div>
           <input
